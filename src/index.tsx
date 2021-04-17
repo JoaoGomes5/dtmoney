8d0877ce6e8357e55b6e7bccad73912aac72999e
Joao Gomes <joaopfg.2002@gmail.com>
2021-04-17 12:46:52 +0100

    :tada: finish chapter II

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer, Model } from 'miragejs';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Website Freelancer',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 3000,
+          createdAt: new Date('2021-04-14 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'University fee',
+          type: 'withdraw',
+          category: 'Scholl',
+          amount: 400,
+          createdAt: new Date('2021-04-31 12:00:00')
+        },
+      ],
+    })
+    
+  },
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema,request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
+    })
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
